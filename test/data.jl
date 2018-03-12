d7364c3bff77ebc1d79fd43bd08a31ef79ba7405
Russell Bent <rbent@lanl.gov>
2018-03-12 08:48:01 -0600

    Added support for status on all components

--- test/data.jl
+++ test/data.jl
@@ -1,13 +1,31 @@
 @testset "connection status = false" begin
-    gm = build_generic_model("../test/data/status.json",  MISOCPGasModel, GasModels.post_gf)
+    gm = build_generic_model("../test/data/status.json",  MISOCPGasModel, GasModels.post_ls)
     @test !haskey(gm.ref[:nw][gm.cnw][:connection], 32)
       
     try 
         gm.var[:nw][gm.cnw][:f][32] == nothing
+        gm.var[:nw][gm.cnw][:f][34] == nothing          
         @test true == false
     catch
     end
     
     @test gm.var[:nw][gm.cnw][:f][14] != nothing
+      
+    try 
+        gm.var[:nw][gm.cnw][:ql][24] == nothing
+        gm.var[:nw][gm.cnw][:ql][29] == nothing          
+        @test true == false
+    catch
+    end
+    
+    @test gm.var[:nw][gm.cnw][:ql][4] != nothing
+      
+    try 
+        gm.var[:nw][gm.cnw][:qg][1] == nothing
+        @test true == false
+    catch
+    end
+    
+    @test gm.var[:nw][gm.cnw][:qg][2] != nothing      
 end      
  
