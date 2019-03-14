358a6f9ef4f509c1671e56be00b2e952783c48b9
Russell Bent <rbent@lanl.gov>
2019-03-14 01:22:56 -0600

    version 6 fix

--- src/form/shared_mi.jl
+++ src/form/shared_mi.jl
@@ -8,7 +8,7 @@ AbstractMIForms = Union{AbstractMISOCPForm, AbstractMINLPForm}
 #######################################################################################################################
 
 "Variables needed for modeling flow in MI models"
-function variable_flow(gm::GenericGasModel, n::Int=gm.cnw; bounded::Bool = true) where T <: AbstractMIForms
+function variable_flow(gm::GenericGasModel{T}, n::Int=gm.cnw; bounded::Bool = true) where T <: AbstractMIForms
     variable_mass_flow(gm,n; bounded=bounded)
     variable_connection_direction(gm,n)
 end
