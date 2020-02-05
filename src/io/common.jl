5810943fc0aa1e4d5404002db57a9f6c6b85a52d
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-02-05 09:06:22 -0700

    additional compressor fields for transient code

--- src/io/common.jl
+++ src/io/common.jl
@@ -40,6 +40,8 @@ function correct_network_data!(data::Dict{String,Any})
     correct_p_mins!(data)
 
     per_unit_data_field_check!(data)
+    add_compressor_fields!(data)
+    
     make_si_units!(data)
     add_base_values!(data)
     make_per_unit!(data)
