330c0c2f8afd46e0a5dc97b8e29ef6da05c6f883
David M Fobes <dfobes@lanl.gov>
2020-02-20 12:08:36 -0700

    Move unneed files / tests to examples folder
    Moves case files in old matlab format, matgas format, and json format
    to examples/data.
    
    Moves long_tests to examples

---
+++ examples/long_tests_gf.jl
@@ -0,0 +1,43 @@
+#Check the second order code model
+@testset "test misocp gf" begin
+    @testset "gaslib 582 case" begin
+        println("gaslib 582 - MISCOP")
+        result = run_gf("../test/data/matgas/gaslib-582.m", MISOCPGasModel, misocp_solver)
+        @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
+        @test isapprox(result["objective"], 0; atol = 1e-6)
+    end
+end
+
+
+# Check the full nonlinear model
+@testset "test minlp gf" begin
+        @testset "gaslib 40 case" begin
+            println("gaslib 40 - MINLP")
+            result = run_gf("../test/data/matgas/gaslib-40.m", MINLPGasModel, minlp_solver)
+            @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
+            @test isapprox(result["objective"], 0; atol = 1e-6)
+        end
+#        @testset "gaslib 135 case" begin
+ #           println("gaslib 135 - MINLP")
+  #          result = run_gf("../examples/data/matlab/gaslib-135.m", MINLPGasModel, minlp_solver)
+   #         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
+    #        @test isapprox(result["objective"], 0; atol = 1e-6)
+   #     end
+end
+
+
+# Check the full nonlinear model
+@testset "test nlp gf" begin
+        @testset "gaslib 40 case" begin
+            println("gaslib 40 - NLP")
+            result = run_gf("../test/data/matgas/gaslib-40.m", NLPGasModel, minlp_solver)
+            @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
+            @test isapprox(result["objective"], 0; atol = 1e-6)
+        end
+#        @testset "gaslib 135 case" begin
+#            println("gaslib 135 - NLP")
+#            result = run_gf("../test/data/matgas/gaslib-135.m", NLPGasModel, minlp_solver)
+#            @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
+#            @test isapprox(result["objective"], 0; atol = 1e-6)
+#        end
+end
