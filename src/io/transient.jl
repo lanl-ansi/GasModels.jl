0304da405e4615aa0e6a426d548d94d30932f95d
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-03-04 13:06:40 -0700

    cleanup of time series block

--- src/io/transient.jl
+++ src/io/transient.jl
@@ -201,6 +201,17 @@ function create_time_series_block(data::Array{Dict{String,Any},1};
         Memento.error(_LOGGER, "the 3600 seconds has to be exactly divisible by the time step, 
         provide a time step that exactly divides 3600.0")
     end 
+    if total_time > 86400.0
+        Memento.warn(_LOGGER, "the solver takes a substantial performance hit when trying to solve 
+        transient optimization problems for more than a day's worth of data; if it takes too long to 
+        converge, please restrict the final time horizon to a day or less")
+    end 
+    if (additional_time == 0.0)
+        Memento.warn(_LOGGER, "the transient optimization problem will only work for time-periodic 
+        time-series data. Please ensure the time-series data is time-periodic with a period of $total_time; 
+        if the data is not time-periodic GasModels will perform a time-periodic spline interpolation if 
+        at least 4 time series data points are available (and result in an error otherwise)")
+    end 
     num_time_points = Int(ceil(end_time / time_step )) + 1
     num_physical_time_points = Int(ceil(total_time / time_step)) + 1
     time_points = collect(LinRange(0.0, end_time, num_time_points))
@@ -210,6 +221,7 @@ function create_time_series_block(data::Array{Dict{String,Any},1};
     time_series_block["time_points"] = time_points 
     time_series_block["time_step"] = time_step
 
+    interpolators = Dict{String,Any}()
     fields = Set()
     for line in data
         type = line["component_type"]
@@ -218,6 +230,48 @@ function create_time_series_block(data::Array{Dict{String,Any},1};
         push!(fields, (type, id, param))
         val = parse(Float64, line["value"])
         timestamp = DateTime(split(line["timestamp"], "+")[1])
+
+        if !haskey(interpolators, type)
+            interpolators[type] = Dict{String,Any}()
+        end
+
+        if !haskey(interpolators[type], id)
+            interpolators[type][id] = Dict{String,Any}()
+        end
+
+        if !haskey(interpolators[type][id], param)
+            interpolators[type][id][param] = Dict{String,Any}(
+                "values" => [],
+                "timestamps" => [],
+                "times" => [],
+                "reduced_data_points" => []
+            )
+        end
+
+        push!(interpolators[type][id][param]["values"], val)
+        push!(interpolators[type][id][param]["timestamps"], timestamp)
+        time_val = (interpolators[type][id][param]["timestamps"][end] - 
+        interpolators[type][id][param]["timestamps"][1]) / Millisecond(1) * 1/1000.0
+        if (time_val <= total_time)
+            push!(interpolators[type][id][param]["times"], time_val)
+            push!(interpolators[type][id][param]["reduced_data_points"], val)
+        end 
+    end
+
+    for (type, id, param) in fields 
+        if (additional_time > 0.0)
+            start_val = interpolators[type][id][param]["reduced_data_points"][1]
+            end_val = interpolators[type][id][param]["reduced_data_points"][end]
+            middle_time = total_time + additional_time/2
+            middle_val = (end_val + start_val)/2
+            push!(interpolators[type][id][param]["times"], middle_time)
+            push!(interpolators[type][id][param]["reduced_data_points"], middle_val)
+            push!(interpolators[type][id][param]["times"], end_time)
+            push!(interpolators[type][id][param]["reduced_data_points"], start_val)
+        end 
+        x = interpolators[type][id][param]["times"]
+        y = interpolators[type][id][param]["reduced_data_points"]
+        interpolators[type][id][param]["itp"] = Spline1D(x, y, periodic=true)
 
         if !haskey(time_series_block, type)
             time_series_block[type] = Dict{String,Any}()
@@ -228,38 +282,14 @@ function create_time_series_block(data::Array{Dict{String,Any},1};
         end
 
         if !haskey(time_series_block[type][id], param)
-            time_series_block[type][id][param] = Dict{String,Any}(
-                "values" => [],
-                "timestamps" => [],
-                "times" => [],
-                "reduced_data_points" => []
-            )
+            time_series_block[type][id][param] = []
         end
 
-        push!(time_series_block[type][id][param]["values"], val)
-        push!(time_series_block[type][id][param]["timestamps"], timestamp)
-        time_val = (time_series_block[type][id][param]["timestamps"][end] - 
-        time_series_block[type][id][param]["timestamps"][1]) / Millisecond(1) * 1/1000.0
-        if (time_val <= total_time)
-            push!(time_series_block[type][id][param]["times"], time_val)
-            push!(time_series_block[type][id][param]["reduced_data_points"], val)
+        itp = interpolators[type][id][param]["itp"]
+        for t in time_series_block["time_points"]
+            push!(time_series_block[type][id][param], itp(t))
         end 
-    end
-
-    for (type, id, param) in fields 
-        start_val = time_series_block[type][id][param]["reduced_data_points"][1]
-        end_val = time_series_block[type][id][param]["reduced_data_points"][end]
-        middle_time = total_time + additional_time/2
-        middle_val = (end_val + start_val)/2
-        push!(time_series_block[type][id][param]["times"], middle_time)
-        push!(time_series_block[type][id][param]["reduced_data_points"], middle_val)
-        push!(time_series_block[type][id][param]["times"], end_time)
-        push!(time_series_block[type][id][param]["reduced_data_points"], start_val)
-        x = time_series_block[type][id][param]["times"]
-        y = time_series_block[type][id][param]["reduced_data_points"]
-        time_series_block[type][id][param]["itp"] = Spline1D(x, y, periodic=true)
     end 
-
 
     return time_series_block
 end 
