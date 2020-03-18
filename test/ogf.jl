3f3b912e82d1d5308a9c29c9b30837d9fd31c8ce
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-03-17 20:41:32 -0600

    Test fixes for case-6 data

--- test/ogf.jl
+++ test/ogf.jl
@@ -1,23 +1,23 @@
 @testset "test ogf" begin
     @testset "test nlp ogf" begin
-        @testset "gaslib 40 nlp ogf" begin
+        @testset "case 6 ogf" begin
             @info "Testing OGF"
             data = GasModels.parse_file("../test/data/matgas/case-6-no-power-limits.m")
             result = run_ogf(data, NLPGasModel, ipopt_solver)
             @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
-            @test isapprox(result["objective"], -15.377535238085375; atol = 1e-2)
+            @test isapprox(result["objective"], -0.0314282; atol = 1e-2)
             GasModels.make_si_units!(result["solution"])
-            @test isapprox(result["solution"]["receipt"]["1"]["fg"], 123.68517579504999; atol = 1e-2)
+            @test isapprox(result["solution"]["receipt"]["1"]["fg"], 123.68219958067358; atol = 1e-2)
         end
 
-        @testset "gaslib 40 nlp ogf binding energy constraint" begin
+        @testset "case 6 nlp ogf binding energy constraint" begin
             @info "Testing OGF Binding Energy Cosntraint"
             data = GasModels.parse_file("../test/data/matgas/case-6.m")
             result = run_ogf(data, NLPGasModel, ipopt_solver)
             @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
-            @test isapprox(result["objective"], -11.56408051038651; atol = 1e-2)
+            @test isapprox(result["objective"], -0.0294033; atol = 1e-2)
             GasModels.make_si_units!(result["solution"])
-            @test isapprox(result["solution"]["receipt"]["1"]["fg"], 97.88470742690157; atol = 1e-2)
+            @test isapprox(result["solution"]["receipt"]["1"]["fg"], 97.33312801519526; atol = 1e-2)
         end
     end
 end
