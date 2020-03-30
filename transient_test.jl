a410431e3b26b9aec64a9226cdabbf428babedef
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-03-30 16:49:24 -0600

    Bug fixes and parsing tests

--- transient_test.jl
+++ transient_test.jl
@@ -2,7 +2,7 @@ using JuMP, Ipopt, GasModels
 ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer)
 
 function main()
-    mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/case6-data/time_series_new.csv", 
+    mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/time-series-case-6b.csv", 
         spatial_discretization=1e4, additional_time=0.0);
 
     return mn_data
