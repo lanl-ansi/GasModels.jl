274f52830c2d4fb288eddd1700f243784af8624f
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-03-31 13:43:49 -0600

    Revert "Bug fix in parsing"
    This reverts commit 34a34271079f04d71cfc7ad238f9d915574813ec.

--- transient_test.jl
+++ transient_test.jl
@@ -1,10 +1,9 @@
 using JuMP, Ipopt, GasModels
-ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "acceptable_tol" => 1e-3)
+ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer)
 
 function main()
- #   mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/time-series-case-6b.csv", 
- #       spatial_discretization=10000.0, additional_time=0.0);
- mn_data = GasModels.parse_files("/Users/kaarthik/Downloads/WebDownloads/KernRiver_mod/KernRiver.m", "/Users/kaarthik/Downloads/WebDownloads/KernRiver_mod/Kern_TransientTimeSeries_cubicsplineinterpolation.csv", spatial_discretization=1e4);
+    mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/time-series-case-6b.csv", 
+        spatial_discretization=10000.0, additional_time=0.0);
 
     return mn_data
 end
