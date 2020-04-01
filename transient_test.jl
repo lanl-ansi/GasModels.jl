27875d375ffe0831890d0f760f2c412c17945c4f
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-04-01 11:02:56 -0600

    Delete transient_test.jl

--- transient_test.jl
+++
@@ -1,9 +0,0 @@
-using JuMP, Ipopt, GasModels
-ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer)
-
-function main()
-    mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/time-series-case-6a.csv", 
-        spatial_discretization=10000.0, additional_time=0.0);
-
-    return mn_data
-end
