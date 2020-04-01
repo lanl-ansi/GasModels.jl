5e5aa41a524b37a36791fa49718a5a199ae7e458
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-04-01 10:53:22 -0600

    Unit tests for transient
    1. Added unit test for transient ogf and parsing
    2. Updated Changelog
    3. Updated Project.toml to drop support for JuMP <= v0.20

--- test/transient.jl
+++ test/transient.jl
@@ -12,6 +12,30 @@
         spatial_discretization=1e4, additional_time=0.0);
     @test length(mn_data["nw"]) == 25
     @test isapprox(mn_data["time_step"] * mn_data["base_time"], 3600.0; atol = 1e-3)
-
 end
+
+@testset "transient (steady state replicate) case" begin
+    mn_data = parse_files("../test/data/matgas/case-6.m", "../test/data/transient/time-series-case-6b.csv", 
+        spatial_discretization=1e4, additional_time=0.0);
+    result = run_transient_ogf(mn_data, NLPGasModel, ipopt_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+    @test isapprox(result["objective"], -5414.35, atol=1e-1)
+    make_si_units!(result["solution"])
+    @test isapprox(result["solution"]["nw"]["1"]["receipt"]["1"]["injection"], 54.97, atol=1e-1)
+    @test isapprox(result["solution"]["nw"]["2"]["receipt"]["1"]["injection"], 99.44, atol=1e-1)
+    @test isapprox(result["solution"]["nw"]["2"]["compressor"]["1"]["power"], 3e6, atol=10)
+end 
+
+@testset "transient time-periodic withdrawal case" begin
+    mn_data = parse_files("../test/data/matgas/case-6.m", "../test/data/transient/time-series-case-6a.csv", 
+        spatial_discretization=1e4, additional_time=0.0);
+    result = run_transient_ogf(mn_data, NLPGasModel, ipopt_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+    @test isapprox(result["objective"], -5207.86, atol=1e-1)
+    make_si_units!(result["solution"])
+    @test isapprox(result["solution"]["nw"]["1"]["receipt"]["1"]["injection"], 43.84, atol=1e-1)
+    @test isapprox(result["solution"]["nw"]["2"]["receipt"]["1"]["injection"], 98.65, atol=1e-1)
+    @test isapprox(result["solution"]["nw"]["2"]["compressor"]["1"]["power"], 3e6, atol=10)
+end 
+
 
--- transient_test.jl
+++ transient_test.jl
@@ -2,7 +2,7 @@ using JuMP, Ipopt, GasModels
 ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer)
 
 function main()
-    mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/time-series-case-6b.csv", 
+    mn_data = GasModels.parse_files("test/data/matgas/case-6.m", "test/data/transient/time-series-case-6a.csv", 
         spatial_discretization=10000.0, additional_time=0.0);
 
     return mn_data
