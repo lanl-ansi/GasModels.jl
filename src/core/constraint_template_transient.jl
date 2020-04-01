bbe85bfccacf8457f4b379ef6012ae4a4199243f
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-03-31 21:05:23 -0600

    Added mass balance constraints

--- src/core/constraint_template_transient.jl
+++ src/core/constraint_template_transient.jl
@@ -1,7 +1,57 @@
-function constraint_slack_node_density(gm::AbstractGasModel, i::Int, nw::Int = gm.cnw)
-    if !haskey(gm.con[:nw][nw], :slack_density)
-        con(gm, nw)[:slack_density] = Dict{Any,JuMP.ConstraintRef}()
+"fixing slack node density value"
+function constraint_slack_junction_density(gm::AbstractGasModel, i::Int, nw::Int = gm.cnw)
+    if !haskey(gm.con[:nw][nw], :slack_junction_density)
+        con(gm, nw)[:slack_junction_density] = Dict{Int,JuMP.ConstraintRef}()
     end
     fixed_density = ref(gm, nw, :slack_junctions, i)["p_nominal"]
-    constraint_slack_node_density(gm, nw, i, fixed_density)
+    constraint_slack_junction_density(gm, nw, i, fixed_density)
+end
+
+"slack junction mass balance"
+function constraint_slack_junction_mass_balance(
+    gm::AbstractGasModel,
+    i::Int,
+    nw::Int = gm.cnw,
+)
+    if !haskey(gm.con[:nw][nw], :slack_junction_mass_balance)
+        con(gm, nw)[:slack_junction_mass_balance] = Dict{Int,JuMP.ConstraintRef}()
+    end
+    net_injection = var(gm, nw, :net_nodal_injection)[i]
+    net_edge_out_flow = var(gm, nw, :net_nodal_edge_out_flow)[i]
+    constraint_slack_junction_mass_balance(gm, nw, i, net_injection, net_edge_out_flow)
+end
+
+"non-slack junction mass balance"
+function constraint_non_slack_junction_mass_balance(
+    gm::AbstractGasModel,
+    i::Int,
+    nw::Int = gm.cnw,
+)
+    if !haskey(gm.con[:nw][nw], :non_slack_junction_mass_balance)
+        con(gm, nw)[:non_slack_junction_mass_balance] = Dict{Int,JuMP.ConstraintRef}()
+    end
+    derivative = var(gm, nw, :non_slack_derivative)[i]
+    net_injection = var(gm, nw, :net_nodal_injection)[i]
+    net_edge_out_flow = var(gm, nw, :net_nodal_edge_out_flow)[i]
+    constraint_non_slack_junction_mass_balance(
+        gm,
+        nw,
+        i,
+        derivative,
+        net_injection,
+        net_edge_out_flow,
+    )
+end
+
+"pipe physics"
+function constraint_pipe_physics_ideal(gm::AbstractGasModel, i::Int, nw::Int = gm.cnw)
+    if !haskey(gm.con[:nw][nw], :pipe_physics_ideal)
+        con(gm, nw)[:pipe_physics_ideal] = Dict{Int,JuMP.ConstraintRef}()
+    end
+    pipe = ref(gm, nw, :pipe, i)
+    fr_junction = pipe["fr_junction"]
+    to_junction = pipe["to_junction"]
+    resistance =
+        pipe["friction_factor"] * gm.ref[:base_length] * pipe["length"] / pipe["diameter"]
+    constraint_pipe_physics_ideal(gm, nw, i, fr_junction, to_junction, resistance)
 end
--- src/core/constraint_transient.jl
+++ src/core/constraint_transient.jl
@@ -1,10 +1,54 @@
-function constraint_slack_node_density(
+"fixing slack node density value"
+function constraint_slack_junction_density(
     gm::AbstractGasModel,
     nw::Int,
     slack_junction_id::Int,
     fixed_density::Float64,
 )
     rho = var(gm, nw, :density, slack_junction_id)
-    con(gm, nw, :slack_density)[slack_junction_id] =
+    con(gm, nw, :slack_junction_density)[slack_junction_id] =
         JuMP.@constraint(gm.model, rho == fixed_density)
+end
+
+"slack junction mass balance"
+function constraint_slack_junction_mass_balance(
+    gm::AbstractGasModel,
+    nw::Int,
+    slack_junction_id::Int,
+    net_injection,
+    net_edge_out_flow,
+)
+    con(gm, nw, :slack_junction_mass_balance)[slack_junction_id] =
+        JuMP.@constraint(gm.model, net_injection == net_edge_out_flow)
+end
+
+"non-slack junction mass balance"
+function constraint_slack_junction_mass_balance(
+    gm::AbstractGasModel,
+    nw::Int,
+    slack_junction_id::Int,
+    derivative,
+    net_injection,
+    net_edge_out_flow,
+)
+    con(gm, nw, :non_slack_junction_mass_balance)[slack_junction_id] = JuMP.@constraint(
+        gm.model,
+        derivative + 4.0 * (net_edge_out_flow - net_injection) == 0
+    )
+end
+
+"pipe physics ideal gas assumption"
+function constraint_pipe_physics_ideal(
+    gm::AbstractGasModel,
+    nw::Int,
+    pipe_id::Int,
+    fr_junction::Int,
+    to_junction::Int,
+    resistance::Float64,
+)
+    p_fr = var(gm, nw, :density, fr_junction)
+    p_to = var(gm, nw, :density, to_junction)
+    f = var(gm, nw, :pipe_flux, i)
+    con(gm, nw, :pipe_physics_ideal)[pipe_id] =
+        JuMP.@NLconstraint(gm.model, p_fr^2 - p_to^2 - resistance * f * abs(f) == 0)
 end
