a51b56135126bbe01001edec335c45a4ecf52283
Byron Tasseff <btasseff@lanl.gov>
2020-06-02 20:05:10 -0600

    More progress on GasLib parser.

--- src/io/gaslib.jl
+++ src/io/gaslib.jl
@@ -7,6 +7,13 @@ function _read_gaslib_file(gaslib_zip, extension::String)
     return XMLDict.xml_dict(subdata_xml)
 end
 
+function _get_compressor_entry(compressor)
+    return Dict{String,Any}("withdrawal_min"=>0.0, "injection_max"=>0.0,
+        "withdrawal_nominal"=>0.0, "is_dispatchable"=>0, "is_per_unit"=>0,
+        "status"=>1, "is_si_units"=>1, "is_english_units"=>0,
+        "compressor_id"=>compressor[:id])
+end
+
 function _get_delivery_entry(delivery)
     injection_max = parse(Float64, delivery["flow"][:value]) * inv(3.6)
     return Dict{String,Any}("withdrawal_min"=>0.0, "injection_max"=>injection_max,
@@ -15,14 +22,9 @@ function _get_delivery_entry(delivery)
 end
 
 function _get_junction_entry(junction)
-    #injection_max = parse(Float64, junction["flow"][:value])
     lat, lon = junction[:geoWGS84Lat], junction[:geoWGS84Long]
     pmin = parse(Float64, junction["pressureMin"][:value]) * 1.0e5
     pmax = parse(Float64, junction["pressureMax"][:value]) * 1.0e5
-
-    if "pseudocriticalPressure" in keys(junction)
-        pmin = parse(Float64, junction["pseudocriticalPressure"][:value]) * 1.0e5
-    end
 
     return Dict("lat"=>lat, "lon"=>lon, "pmin"=>pmin, "pmax"=>pmax,
                 "is_dispatchable"=>0, "is_per_unit"=>0, "status"=>1,
@@ -30,11 +32,30 @@ function _get_junction_entry(junction)
                 "id"=>junction[:id], "index"=>junction[:id], "pipeline_id"=>"")
 end
 
+function _get_pipe_entry(pipe)
+    fr_junction = pipe[:from]
+    to_junction = pipe[:to]
+    p_max = parse(Float64, pipe["pressureMax"][:value]) * 1.0e5
+    diameter = parse(Float64, pipe["diameter"][:value]) * inv(1000.0)
+    length = parse(Float64, pipe["length"][:value]) * 1000.0
+
+    return Dict{String,Any}("fr_junction"=>fr_junction, "to_junction"=>to_junction,
+        "diameter"=>diameter, "length"=>length, "is_per_unit"=>0, "status"=>1,
+        "is_si_units"=>1, "is_english_units"=>0, "junction_id"=>pipe[:id],
+        "p_max"=>p_max)
+end
+
 function _get_receipt_entry(receipt)
     injection_max = parse(Float64, receipt["flow"][:value]) * inv(3.6)
     return Dict{String,Any}("injection_min"=>0.0, "injection_max"=>injection_max,
         "injection_nominal"=>injection_max, "is_dispatchable"=>0, "is_per_unit"=>0,
         "status"=>1, "is_si_units"=>1, "is_english_units"=>0, "junction_id"=>receipt[:id])
+end
+
+function _get_gaslib_compressors(topology, compressor_stations)
+    compressors = topology["network"]["connections"]["compressorStation"]
+    compressors = Dict(x[:id] => x for x in compressors)
+    return Dict(i => _get_compressor_entry(x) for (i, x) in compressors)
 end
 
 function _get_gaslib_deliveries(topology, nomination)
@@ -52,6 +73,12 @@ function _get_gaslib_junctions(topology)
     return Dict(i => _get_junction_entry(x) for (i, x) in junctions)
 end
 
+function _get_gaslib_pipes(topology)
+    pipes = topology["network"]["connections"]["pipe"]
+    pipes = Dict(x[:id] => x for x in pipes)
+    return Dict(i => _get_pipe_entry(x) for (i, x) in pipes)
+end
+
 function _get_gaslib_receipts(topology, nomination)
     ids = [x[:id] for x in topology["network"]["nodes"]["source"]]
     scenario = nomination["boundaryValue"]["scenario"]
@@ -67,7 +94,12 @@ function parse_gaslib(zip_path::Union{IO, String})
     compressor_stations = _read_gaslib_file(gaslib_zip, ".cs")
 
     # Create a dictionary for receipt components.
+    compressors = _get_gaslib_compressors(topology, compressor_stations)
     deliveries = _get_gaslib_deliveries(topology, nominations)
     junctions = _get_gaslib_junctions(topology)
     receipts = _get_gaslib_receipts(topology, nominations)
+    pipes = _get_gaslib_pipes(topology)
+
+    return Dict{String,Any}("compressor"=>compressors, "delivery"=>deliveries,
+        "junction"=>junctions, "receipt"=>receipts, "pipe"=>pipes)
 end
