50c34a1711e26437dd13d2da925c5fb67d8adae6
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-08-26 08:48:52 -0600

    WIP storage: variables added

--- src/core/constraint_transient.jl
+++ src/core/constraint_transient.jl
@@ -137,3 +137,101 @@ function constraint_compressor_power(
         JuMP.@NLconstraint(gm.model, compressor_power <= power_max)
     )
 end
+
+"Constraint: storage effective flow" 
+function constraint_storage_well_head_to_eff_flow(gm::AbstractGasModel, storage_id::Int, nw::Int = gm.cnw)
+    f_eff = var(gm, nw, :storage_effective, storage_id)
+    f_wh = var(gm, nw, :well_head, storage_id)
+    _add_constraint!(
+        gm, 
+        nw, 
+        :storage_effective_flow_balance,
+        storage_id,
+        JuMP.@constraint(gm.model, f_eff == f_wh)
+    )
+end 
+
+"Constraint: well compression/pressure-reduction"
+function constraint_storage_compressor_regulator(
+    gm::AbstractGasModel,
+    nw::Int,
+    storage_id::Int,
+    junction_id::Int,
+)
+    rho_junction = var(gm, nw, :density, junction_id)
+    rho_well_head = var(gm, nw, :well_density, storage_id)[1]
+    alpha = var(gm, nw, :storage_compressor_ratio, storage_id)
+    GasModels._add_constraint!(
+    gm,
+    nw,
+    :well_compressor_regulator,
+    storage_id,
+    JuMP.@constraint(gm.model, rho_junction == alpha * rho_well_head)
+    )
+end
+
+"Constraint: well momentum balance"
+function constraint_storage_well_momentum_balance(
+    gm::AbstractGasModel, 
+    nw::Int, 
+    num_discretizations::Int, 
+    storage_id::Int, 
+    beta::Float64, 
+    resistance::Float64, 
+)
+    for i in 1:num_discretizations
+        rho_top = var(gm, nw, :well_density, storage_id)[i]
+        rho_bottom = var(gm, nw, :well_density, storage_id)[i+1]
+        phi_avg = var(gm, nw, :well_flux_avg, storage_id)[i]
+        GasModels._add_constraint!(
+            gm, 
+            nw, 
+            :well_ideal_momentum_balance,
+            storage_id * 1000 + i,
+            JuMP.@NLconstraint(gm.model, exp(beta) * rho_top^2 - rho_bottom^2 == (-resistance * phi_avg * abs(phi_avg)) * (exp(beta) - 1) / beta)
+        )
+    end 
+end
+
+"Constraint: well mass balance"
+function constraint_storage_well_mass_balance(
+    gm::AbstractGasModel, 
+    nw::Int, 
+    num_discretizations::Int, 
+    storage_id::Int, 
+    length::Float64 
+)
+    for i in 1:num_discretizations
+        rho_dot_top = var(gm, nw, :well_density_derivative, storage_id)[i]
+        rho_dot_bottom = var(gm, nw, :well_density_derivative, storage_id)[i+1]
+        phi_neg = var(gm, nw, :well_flux_neg, storage_id)[i]
+        GasModels._add_constraint!(
+            gm, 
+            nw, 
+            :well_ideal_mass_balance, 
+            storage_id * 1000 + i, 
+            JuMP.@constraint(gm.model, length * (rho_dot_top + rho_dot_bottom) = -4 * phi_neg)
+        )
+    end 
+end
+
+"Constraint: storage well nodal balance"
+function constraint_storage_well_nodal_balance(gm::AbstractGasModel, storage_id::Int, nw::Int = gm.cnw; 
+    num_discretizations::Int = 4)
+    
+    for i in 1:(num_discretizations-1)
+        phi_top_well_avg = var(gm, nw, :well_flux_avg, storage_id)[i]
+        phi_top_well_neg = var(gm, nw, :well_flux_neg, storage_id)[i]
+        phi_bottom_well_avg = var(gm, nw, :well_flux_avg, storage_id)[i+1]
+        phi_bottom_well_neg = var(gm, nw, :well_flux_neg, storage_id)[i+1]
+
+        GasModels._add_constraint!(
+            gm,
+            nw,
+            :well_nodal_balance,
+            storage_id* 1000 + i,
+            JuMP.@constraint(gm.model, phi_top_well_avg - phi_top_well_neg == phi_bottom_well_avg + phi_bottom_well_neg)
+            )
+
+    end
+end 
