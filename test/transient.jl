81395619fdf4b51ebcbe101b9db0a1328edef85b
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-09-01 21:40:23 -0600

    added transient storage tests

--- test/transient.jl
+++ test/transient.jl
@@ -77,3 +77,69 @@ end
         atol = 10,
     )
 end
+
+@testset "transient (steady state replicate) case with storage" begin
+    mn_data = parse_files(
+        "../test/data/matgas/case-6-storage.m",
+        "../test/data/transient/time-series-case-6b.csv",
+        spatial_discretization = 1e4,
+        additional_time = 3600.0,
+    )
+    result = run_transient_ogf(mn_data, WPGasModel, nlp_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+    @test isapprox(result["objective"], -9491, atol = 1)
+    make_si_units!(result["solution"])
+    @test isapprox(
+        result["solution"]["nw"]["1"]["receipt"]["1"]["injection"],
+        44,
+        atol = 1,
+    )
+    @test isapprox(
+        result["solution"]["nw"]["2"]["receipt"]["1"]["injection"],
+        44,
+        atol = 1,
+    )
+    @test isapprox(
+        result["solution"]["nw"]["1"]["storage"]["1"]["withdrawal"],
+        -86,
+        atol = 1,
+    )
+    @test isapprox(
+        result["solution"]["nw"]["2"]["storage"]["1"]["withdrawal"],
+        -86,
+        atol = 1,
+    )
+end
+
+@testset "transient time-periodic withdrawal case with storage" begin
+    mn_data = parse_files(
+        "../test/data/matgas/case-6-storage.m",
+        "../test/data/transient/time-series-case-6a.csv",
+        spatial_discretization = 1e4,
+        additional_time = 0.0,
+    )
+    result = run_transient_ogf(mn_data, WPGasModel, nlp_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+    @test isapprox(result["objective"], -9282, atol = 1)
+    make_si_units!(result["solution"])
+    @test isapprox(
+        result["solution"]["nw"]["1"]["receipt"]["1"]["injection"],
+        0,
+        atol = 1e-1,
+    )
+    @test isapprox(
+        result["solution"]["nw"]["2"]["receipt"]["1"]["injection"],
+        0.0,
+        atol = 1e-1,
+    )
+    @test isapprox(
+        result["solution"]["nw"]["1"]["storage"]["1"]["withdrawal"],
+        -83,
+        atol = 1,
+    )
+    @test isapprox(
+        result["solution"]["nw"]["2"]["storage"]["1"]["withdrawal"],
+        -83,
+        atol = 1,
+    )
+end
