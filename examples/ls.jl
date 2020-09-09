527fc976fa672a43c9473caffc40a478b7b076be
David M Fobes <dfobes@lanl.gov>
2020-09-09 15:32:27 -0600

    FIX: examples

--- examples/ls.jl
+++ examples/ls.jl
@@ -5,11 +5,7 @@
         @info "Testing gaslib crdwp ls gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls.m", CRDWPGasModel, misocp_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            497.3;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 497.3; atol = 1e-1)
     end
 
     #Check the second order cone model on load shedding with priorities
@@ -17,11 +13,7 @@
         @info "Testing gaslib crdwp ls priority gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls-priority.m", CRDWPGasModel, misocp_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            447.54;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 447.54; atol = 1e-1)
     end
 
     #Check the lrdwp model on load shedding
@@ -29,11 +21,7 @@
         @info "Testing gaslib lrdwp ls gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls.m", LRDWPGasModel, mip_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            838.76;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 838.76; atol = 1e-1)
     end
 
     #Check the lrdwp model on load shedding with priorities
@@ -41,11 +29,7 @@
         @info "Testing gaslib lrdwp ls priority gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls-priority.m", LRDWPGasModel, mip_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            754.88;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 754.88; atol = 1e-1)
     end
 
     #Check the lrwp model on load shedding
@@ -53,11 +37,7 @@
         @info "Testing gaslib lrwp ls gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls.m", LRWPGasModel, lp_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            838.75;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 838.75; atol = 1e-1)
     end
 
     #Check the lrwp model on load shedding with priorities
@@ -65,11 +45,7 @@
         @info "Testing gaslib lrwp ls priority gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls-priority.m", LRWPGasModel, lp_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            754.88;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 754.88; atol = 1e-1)
     end
 
     #Check the wp model on load shedding
@@ -77,11 +53,7 @@
         @info "Testing gaslib wp ls gaslib 40"
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls.m", WPGasModel, lp_solver)
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            420.91;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 420.91; atol = 1e-1)
         @warn(result["termination_status"])
     end
 
@@ -91,10 +63,6 @@
         result = run_ls("../examples/data/matgas/gaslib-40-E-ls-priority.m", WPGasModel, nlp_solver)
         println(result["termination_status"])
         @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL || result["termination_status"] == :Suboptimal
-        @test isapprox(
-            result["objective"] * result["solution"]["base_flow"],
-            378.82;
-            atol = 1e-1,
-        )
+        @test isapprox(result["objective"] * result["solution"]["base_flow"], 378.82; atol = 1e-1)
     end
 end
--- examples/ne_A3.jl
+++ examples/ne_A3.jl
@@ -3,32 +3,28 @@
     @testset "A3 dwp ne" begin
         @info "Testing A3 dwp ne"
         result = run_ne("../examples/data/matgas/A3.m", DWPGasModel, minlp_solver)
-        @test result["termination_status"] == LOCALLY_SOLVED ||
-              result["termination_status"] == OPTIMAL
+        @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
         @test isapprox(result["objective"], 1781; atol = 1e-1)
     end
 
     @testset "A3 wp ne" begin
         @info "Testing A3 wp ne"
         result = run_ne("../examples/data/matgas/A3.m", WPGasModel, minlp_solver)
-        @test result["termination_status"] == LOCALLY_SOLVED ||
-              result["termination_status"] == OPTIMAL
+        @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
         @test isapprox(result["objective"], 1781; atol = 1e-1)
     end
 
     @testset "A3 crdwp case" begin
         @info "Testing A3 crdwp ne"
         result = run_ne("../examples/data/matgas/A3.m", CRDWPGasModel, misocp_solver)
-        @test result["termination_status"] == LOCALLY_SOLVED ||
-              result["termination_status"] == OPTIMAL
+        @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
         @test isapprox(result["objective"], 1781; atol = 1e-1)
     end
 
     @testset "A3 lrwp case" begin
         @info "Testing A3 lrwp ne"
         result = run_ne("../examples/data/matgas/A3.m", LRWPGasModel, mip_solver)
-        @test result["termination_status"] == LOCALLY_SOLVED ||
-              result["termination_status"] == OPTIMAL
+        @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
         @test isapprox(result["objective"], 0.0; atol = 1e-1)
         GC.gc()
     end
@@ -36,8 +32,7 @@
     @testset "A3 lrdwp case" begin
         @info "Testing A3 lrdwp ne"
         result = run_ne("../examples/data/matgas/A3.m", LRDWPGasModel, mip_solver)
-        @test result["termination_status"] == LOCALLY_SOLVED ||
-              result["termination_status"] == OPTIMAL
+        @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
         @test isapprox(result["objective"], 0.0; atol = 1e-1)
     end
 
