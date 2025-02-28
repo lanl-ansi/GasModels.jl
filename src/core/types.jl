5817ee3ed7df93e8dc997158b8302c63430179a8
Russell Bent <rbent@lanl.gov>
2025-02-28 16:43:00 -0700

    implementation for network expansion

--- src/core/types.jl
+++ src/core/types.jl
@@ -2,14 +2,11 @@
 "DWP models"
 abstract type AbstractDWPModel <: AbstractGasModel end
 
-
 "LRDWP models"
 abstract type AbstractLRDWPModel <: AbstractGasModel end
 
-
 "CRDWP models"
 abstract type AbstractCRDWPModel <: AbstractGasModel end
-
 
 "WP models"
 abstract type AbstractWPModel <: AbstractGasModel end
@@ -23,18 +20,14 @@ abstract type AbstractLRWPModel <: AbstractGasModel end
 "LRWP Model Type"
 mutable struct LRWPGasModel <: AbstractLRWPModel @gm_fields end
 
-
 "LRDWP Model Type"
 mutable struct LRDWPGasModel <: AbstractLRDWPModel @gm_fields end
-
 
 "WP Model Type"
 mutable struct WPGasModel <: AbstractWPModel @gm_fields end
 
-
 "CRDWP Model Type"
 mutable struct CRDWPGasModel <: AbstractCRDWPModel @gm_fields end
-
 
 "DWP Model Type"
 mutable struct DWPGasModel <: AbstractDWPModel @gm_fields end
