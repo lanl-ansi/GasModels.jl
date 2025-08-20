6a4392192da2e2bf63bd531dbd4697b3fd5e83ce
Skyler Reid <96967651+skylerreid@users.noreply.github.com>
2025-08-20 11:29:50 -0600

    Update nels.jl

--- test/nels.jl
+++ test/nels.jl
@@ -1,42 +1,42 @@
 @testset "test nels" begin
     @testset "test crdwp nels" begin
         @info "Testing crdwp nels"
-        result = run_nels("../test/data/matgas/case-6-nels.m", CRDWPGasModel, misocp_solver)
+        result = solve_nels("../test/data/matgas/case-6-nels.m", CRDWPGasModel, misocp_solver)
         @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
         @test isapprox(result["objective"] * result["solution"]["base_flow"], 1031.60; atol = 1e-1)
     end
 
     @testset "test dwp nels" begin
         @info "Testing dwp nels"
-        result = run_nels("../test/data/matgas/case-6-nels.m", DWPGasModel, minlp_solver)
+        result = solve_nels("../test/data/matgas/case-6-nels.m", DWPGasModel, minlp_solver)
         @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
         @test isapprox(result["objective"] * result["solution"]["base_flow"], 1031.60; atol = 1e-1)
     end
 
     @testset "test lrdwp nels" begin
         @info "Testing lrdwp nels"
-        result = run_nels("../test/data/matgas/case-6-nels.m", LRDWPGasModel, mip_solver)
+        result = solve_nels("../test/data/matgas/case-6-nels.m", LRDWPGasModel, mip_solver)
         @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
         @test isapprox(result["objective"] * result["solution"]["base_flow"], 1036.93; atol = 1e-1)
     end
 
     @testset "test lrwp nels" begin
         @info "Testing lrwp nels"
-        result = run_nels("../test/data/matgas/case-6-nels.m", LRWPGasModel, mip_solver)
+        result = solve_nels("../test/data/matgas/case-6-nels.m", LRWPGasModel, mip_solver)
         @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
         @test isapprox(result["objective"] * result["solution"]["base_flow"], 1036.93; atol = 1e-1)
     end
 
     @testset "test wp nels" begin
         @info "Testing wp nels"
-        result = run_nels("../test/data/matgas/case-6-nels.m", WPGasModel, minlp_solver)
+        result = solve_nels("../test/data/matgas/case-6-nels.m", WPGasModel, minlp_solver)
         @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
         @test isapprox(result["objective"] * result["solution"]["base_flow"], 1031.60; atol = 1e-1)
     end
 
     @testset "test cwp nels" begin
         @info "Testing cwp nels"
-        result = run_nels("../test/data/matgas/case-6-nels.m", CWPGasModel, minlp_solver)
+        result = solve_nels("../test/data/matgas/case-6-nels.m", CWPGasModel, minlp_solver)
         @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
         @test isapprox(result["objective"] * result["solution"]["base_flow"], 1031.60; atol = 1e-1)
     end
