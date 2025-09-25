40589ae934d70b070d4993e48c5062993c1caa64
Skyler Reid <skyler.reid2235@gmail.com>
2025-09-25 08:47:17 -0600

    fixed tests again

--- test/storage.jl
+++ test/storage.jl
@@ -1,13 +1,13 @@
 @testset "Steady State Storage" begin
     @info "Testing GF Storage"
-    result = run_gf("../test/data/matgas/case-6-storage.m", CRDWPGasModel, misocp_solver)
+    result = solve_gf("../test/data/matgas/case-6-storage.m", CRDWPGasModel, misocp_solver)
     @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
-    result = run_gf("../test/data/matgas/case-6-storage.m", DWPGasModel, minlp_solver)
+    result = solve_gf("../test/data/matgas/case-6-storage.m", DWPGasModel, minlp_solver)
     @test result["termination_status"] in [ALMOST_LOCALLY_SOLVED, LOCALLY_SOLVED, OPTIMAL]
-    result = run_gf("../test/data/matgas/case-6-storage.m", WPGasModel, minlp_solver)
+    result = solve_gf("../test/data/matgas/case-6-storage.m", WPGasModel, minlp_solver)
     @test result["termination_status"] in [ALMOST_LOCALLY_SOLVED, LOCALLY_SOLVED, OPTIMAL]
-    result = run_gf("../test/data/matgas/case-6-storage.m", LRDWPGasModel, mip_solver)
+    result = solve_gf("../test/data/matgas/case-6-storage.m", LRDWPGasModel, mip_solver)
     @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
-    result = run_gf("../test/data/matgas/case-6-storage.m", LRWPGasModel, mip_solver)
+    result = solve_gf("../test/data/matgas/case-6-storage.m", LRWPGasModel, mip_solver)
     @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == OPTIMAL
 end
