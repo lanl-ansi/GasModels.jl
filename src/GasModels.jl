b7df9f5a5101efe854ff16fbbffc1a919cf1ab35
skylerreid <skyler.reid2235@gmail.com>
2025-11-13 10:50:26 -0700

    move build_mnw into a new branch for pr

--- src/GasModels.jl
+++ src/GasModels.jl
@@ -52,6 +52,7 @@ module GasModels
     include("io/gaslib.jl")
     include("io/matgas.jl")
     include("io/transient.jl")
+    include("io/build_multinetwork.jl")
 
     include("core/base.jl")
     include("core/types.jl")
