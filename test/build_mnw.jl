f2f3331ddf5125f7aa60aebcd0e406f346065ce4
skylerreid <skyler.reid2235@gmail.com>
2025-12-01 16:35:37 -0700

    add tests and fix includes - all tests pass but some warnings

--- test/build_mnw.jl
+++ test/build_mnw.jl
@@ -13,3 +13,22 @@ end
     result = solve_transient_ogf(mn_data, WPGasModel, nlp_solver)
     @test result["termination_status"] == LOCALLY_SOLVED
 end
+
+@testset "test elevation case" begin
+    mn_data = build_multinetwork("../test/data/matgas/case-6-elevation.m", "../test/data/transient/time-series-case-6a.csv", time_step=864.0)
+    result = solve_transient_ogf(mn_data, WPGasModel, nlp_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+end
+
+@testset "test ls-priority case" begin
+    mn_data = build_multinetwork("../test/data/matgas/case-6-ls-priority.m", "../test/data/transient/time-series-case-6a.csv", time_step=864.0)
+    result = solve_transient_ogf(mn_data, WPGasModel, nlp_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+end
+
+#lots of IM warnings associated with this one - WIP
+@testset "test no limits case" begin
+    mn_data = build_multinetwork("../test/data/matgas/case-6-no-power-limits.m", "../test/data/transient/time-series-case-6a.csv", time_step=864.0)
+    result = solve_transient_ogf(mn_data, WPGasModel, nlp_solver)
+    @test result["termination_status"] == LOCALLY_SOLVED
+end
