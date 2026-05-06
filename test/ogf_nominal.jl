aae50c027e6785a64413601ccc41d1e812f68b61
Sai Krishna Kanth Hari <hskkanth@gmail.com>
2026-05-06 13:53:11 -0600

    Updating nominal OGF behavior for transfers
    Updated the upper and lower bounds for transfers to be based on the sign of nominal values
    
    Updated the objective prices to be based on the sign of nominal transfer values
    
    Updated constraint_mass_flow_balance to use nominal values instead of min/max
    
    Added tests

--- test/ogf_nominal.jl
+++ test/ogf_nominal.jl
@@ -1,4 +1,26 @@
 @testset "test ogf nominal" begin
+    @testset "nominal dispatch variable bounds" begin
+        data = GasModels.parse_file("../test/data/matgas/case-6-nels.m")
+        transfer = data["transfer"]["1"]
+        transfer["is_dispatchable"] = 1
+        transfer["withdrawal_min"] = -12.0
+        transfer["withdrawal_max"] = 30.0
+        transfer["withdrawal_nominal"] = -5.0
+        gm = GasModels.instantiate_model(data, WPGasModel, GasModels.build_ogf_nominal)
+
+        receipt = ref(gm, :receipt, 1)
+        delivery = ref(gm, :delivery, 1)
+        fg = var(gm, :fg)[1]
+        fl = var(gm, :fl)[1]
+        ft = var(gm, :ft)[1]
+
+        @test (JuMP.lower_bound(fg), JuMP.upper_bound(fg), JuMP.start_value(fg)) ==
+              (receipt["injection_min"], receipt["injection_nominal"], receipt["injection_nominal"])
+        @test (JuMP.lower_bound(fl), JuMP.upper_bound(fl), JuMP.start_value(fl)) ==
+              (delivery["withdrawal_min"], delivery["withdrawal_nominal"], delivery["withdrawal_nominal"])
+        @test (JuMP.lower_bound(ft), JuMP.upper_bound(ft), JuMP.start_value(ft)) == (-5.0, 0.0, -5.0)
+    end
+
     @testset "test wp ogf nominal" begin
         @testset "case 6 ogf nominal" begin
             @_info "Testing OGF Nominal"
@@ -8,6 +30,18 @@
             @test isapprox(result["objective"], -253.683; atol = 1e-2)
             GasModels.make_si_units!(result["solution"])
             @test isapprox(result["solution"]["receipt"]["1"]["fg"], 123.6821; atol = 1e-2)
+        end
+
+        @testset "case 6 ogf nominal modified transfer" begin
+            @_info "Testing OGF Nominal Modified transfer"
+            data = GasModels.parse_file("../test/data/matgas/case-6-no-power-limits-nominal.m")
+            transfer = data["transfer"]["1"]
+            transfer["withdrawal_nominal"] = - transfer["withdrawal_max"] / 2.0
+
+            result = solve_ogf_nominal(data, WPGasModel, nlp_solver)
+            GasModels.make_si_units!(result["solution"])
+            @test result["termination_status"] in [LOCALLY_SOLVED, ALMOST_LOCALLY_SOLVED, OPTIMAL, :Suboptimal]
+            @test isapprox(result["solution"]["transfer"]["1"]["ft"], -15.0; atol = 1e-2)
         end
 
         @testset "case 6 ogf nominal" begin
